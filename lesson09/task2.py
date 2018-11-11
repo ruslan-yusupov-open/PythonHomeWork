838a91c6ce9eb24938b235b93ca0da65ec9c05c9
Puten <afaputen@list.ru>
2018-11-11 13:00:46 +0300

    descriptions

--- lesson09/task2.py
+++ lesson09/task2.py
@@ -1,47 +1,50 @@
 # при помощи библиотеки requests нужно скачать содержимое страниц
-# reddit.com/r/python (любой тред &gt; 5 комментариев) и вывести пару: автор
+# reddit.com/r/python (любой тред > 5 комментариев) и вывести пару: автор
 # комментария и его текст в консоль
 
 # -*- coding: utf-8 -*-
 
 import requests
 
+MIN_COMMENTS = 5
+
 REQUEST_HEADERS = {
     'User-agent': 'Mozilla/5.0'
 }
 
+
 def get_reddit():
     r = requests.get('http://reddit.com/r/python', headers=REQUEST_HEADERS)
-    # print(r.status_code)
-    # print(r.headers)
-    # print()
 
     import re
 
     pattern = r'<a [^>]+? href=\"([^\">]+?)\" [^>]+?>.*?(\d+) comments'
 
-    print(str(r.content))
+    # print(str(r.content))
 
     result = re.findall(pattern, str(r.content))
 
-    print(result)
+    url = None
+
+    for res_item in result:
+        if int(res_item[1]) >= MIN_COMMENTS:
+            url = res_item[0]
+            print(res_item)
+            break
+
+    return url
+
+
+def get_comments(url):
+    r = requests.get('http://reddit.com/r/python', headers=REQUEST_HEADERS)
+
+    r2 = requests.get(url, headers=REQUEST_HEADERS)
+
+    print(str(r2.content))
 
 
 if __name__ == '__main__':
-    get_reddit()
+    #    url = get_reddit()
+    page_url = "https://www.reddit.com/r/Python/comments/9vsvou/python_job_opportunities_and_a_little_roadmap/"
 
-# find_pet_by_status('sold')
 
-
-# def find_pet_by_status(status):
-#     params = {'status': status}
-#     headers = {
-#         #'Accept': 'application/xml'
-#         'Accept': 'application/json'
-#     }
-#     url = 'http://petstore.swagger.io/v2/pet/findByStatus'
-#     r = requests.get(url, params=params, headers=headers)
-#     print(r.status_code, r.headers)
-#     print(r.content)
-#
-#     # s = 'http://petstore.swagger.io/v2/pet/findByStatus?status=sold'
