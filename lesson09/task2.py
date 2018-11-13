0f2c8750720a82e1bace5236208cf57db38ca48e
Puten <afaputen@list.ru>
2018-11-13 14:06:03 +0300

    updated

--- lesson09/task2.py
+++ lesson09/task2.py
@@ -3,7 +3,7 @@
 # комментария и его текст в консоль
 
 # -*- coding: utf-8 -*-
-
+import re
 import requests
 
 MIN_COMMENTS = 5
@@ -20,31 +20,43 @@ def get_reddit():
 
     pattern = r'<a [^>]+? href=\"([^\">]+?)\" [^>]+?>.*?(\d+) comments'
 
-    # print(str(r.content))
-
     result = re.findall(pattern, str(r.content))
+    result = list(filter(lambda item: (int(item[1]) > MIN_COMMENTS), result))
 
-    url = None
-
-    for res_item in result:
-        if int(res_item[1]) >= MIN_COMMENTS:
-            url = res_item[0]
-            print(res_item)
-            break
-
-    return url
+    if len(result) > 0:
+        res_item = result[0]
+        print("url {}".format(res_item[0]))
+        return res_item[0]
+    else:
+        return None
 
 
-def get_comments(url):
-    r = requests.get('http://reddit.com/r/python', headers=REQUEST_HEADERS)
+def print_comments(url):
+    # лайфхак - используем старую версию реддита, которая чуть более понятна
+    url = url.replace("https://www.reddit.com", "https://old.reddit.com")
 
     r2 = requests.get(url, headers=REQUEST_HEADERS)
 
-    print(str(r2.content))
+    str_content = str(r2.content)
+
+    search = re.search(r'<div class="content" role="main"[^>]*?>(.+?)<div class="footer-parent"[^>]*?>', str_content)
+
+    if search:
+        str_content = search.group(1)
+
+    name_pattern_group = r'<a href="[^"]+/user/([^"]+)"[^>]*?class="author'  # [^"+]"[^>]*?>([^<]*?)</a>'
+    names = re.findall(name_pattern_group, str_content)
+
+    name_pattern_group = r'<div class="usertext-body[^>]*?>[^<]*?<div class="md">(.+?)</div>'
+    comments = re.findall(name_pattern_group, str_content)
+
+    names_comments = zip(names, comments)
+    for name_comment in names_comments:
+        print("name: {}, comment: {}".format(name_comment[0], name_comment[1]))
 
 
 if __name__ == '__main__':
-    #    url = get_reddit()
-    page_url = "https://www.reddit.com/r/Python/comments/9vsvou/python_job_opportunities_and_a_little_roadmap/"
+    site_url = get_reddit()
 
-
+    if site_url:
+        print_comments(site_url)
