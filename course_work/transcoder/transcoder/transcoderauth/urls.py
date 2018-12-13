ad34feff11f48176ea4c7e66f4a3db104775bfea
Puten <afaputen@list.ru>
2018-12-13 17:29:57 +0300

    some login forms

--- course_work/transcoder/transcoder/transcoderauth/urls.py
+++ course_work/transcoder/transcoder/transcoderauth/urls.py
@@ -1,11 +1,23 @@
+from django.contrib.auth.views import LoginView, logout_then_login
 from django.urls import path
+from django.views.generic import CreateView
 
+from transcoderauth.forms import CustomCreationForm
 from transcoderauth.views import login, register, logout
 
 app_name = 'transcoderauth'
 
 urlpatterns = [
-    path('login/', login, name='login'),
-    path('register/', register, name='register'),
-    path('logout/', logout, name='logout')
+
+    path('login/', LoginView.as_view(
+        template_name='login.html'
+    ), name='login'),
+
+    path('logout/', logout_then_login, name='logout'),
+
+    path('register/', CreateView.as_view(
+        template_name='register.html',
+        form_class=CustomCreationForm,
+        success_url='/',
+    ), name='register'),
 ]
